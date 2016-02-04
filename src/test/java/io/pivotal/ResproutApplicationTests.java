e99d056f88242b0a725cfa6e19cdfb02e3ea4781
Michael Freedman <pair+mfreedman@pivotallabs.com>
2016-02-04 13:23:31 +1100

    init application

---
+++ src/test/java/io/pivotal/ResproutApplicationTests.java
@@ -0,0 +1,16 @@
+package io.pivotal;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ResproutApplication.class)
+public class ResproutApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
