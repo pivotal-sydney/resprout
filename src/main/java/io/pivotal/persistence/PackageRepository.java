e43d4d5af99455c56f066dc08be2e2ab9eb8c180
Michael Freedman <pair+mfreedman@pivotallabs.com>
2016-02-15 11:18:18 +1100

    allow for searching by name

--- src/main/java/io/pivotal/persistence/PackageRepository.java
+++ src/main/java/io/pivotal/persistence/PackageRepository.java
@@ -3,8 +3,12 @@ package io.pivotal.persistence;
 
 import io.pivotal.model.Package;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
 
 @RepositoryRestResource(collectionResourceRel = "packages", path = "packages")
 public interface PackageRepository extends PagingAndSortingRepository<Package, Long> {
+    public List<Package> findByNameContainingIgnoreCase(@Param("name") String name);
 }
